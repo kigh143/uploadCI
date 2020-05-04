6fb6f198b76c352368f09ada65a5672feff51ad5
Katende hakim. G <ghkatende@gmail.com>
2020-05-04 12:39:04 +0300

    the service file

---
+++ service.ts
@@ -0,0 +1,17 @@
+import { HttpClient, HttpEvent, HttpErrorResponse, HttpEventType } from  '@angular/common/http';
+import { map } from  'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadService {
+
+  SERVER_URL: string = "http://127.0.0.1:8000/";
+
+  constructor(private httpClient: HttpClient) { }
+  
+   public uploadFile(data) {
+    let uploadURL = `${this.SERVER_URL}/upload.php`;
+    return this.httpClient.post<any>(uploadURL, data);
+  }
+}
