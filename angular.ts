4d782fdc8c2c138bdd21c504fabcfdbcbd076634
Katende hakim. G <ghkatende@gmail.com>
2020-05-04 12:37:52 +0300

    added ts code

---
+++ angular.ts
@@ -0,0 +1,37 @@
+import { FormBuilder, FormGroup } from  '@angular/forms';
+import { UploadService } from  '../upload.service';
+
+export class ProfileComponent implements OnInit {
+
+  form: FormGroup;
+  uploadResponse;
+
+  constructor(private formBuilder: FormBuilder, private uploadService: UploadService) { }
+  
+    ngOnInit() {
+    this.form = this.formBuilder.group({
+      avatar: ['']
+    });
+  }
+  
+    onFileSelect(event) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.form.get('avatar').setValue(file);
+    }
+  }
+  
+   onSubmit() {
+    const formData = new FormData();
+    formData.append('avatar', this.form.get('avatar').value);
+
+    this.uploadService.uploadFile(formData).subscribe(
+      (res) => {
+        this.uploadResponse = res;
+          console.log(res);
+      },
+      (err) => {  
+        console.log(err);
+      }
+    );
+  }
